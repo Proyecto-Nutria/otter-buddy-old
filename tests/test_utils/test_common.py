ecc21803cb88501cdff7be2a4fc93c58bf9d793f
akotadi <manuel_7795@hotmail.com>
2021-06-21 13:55:31 -0500

    Add pretty_time_format tests

--- tests/test_utils/test_common.py
+++ tests/test_utils/test_common.py
@@ -1,3 +1,5 @@
+import time
+
 from otter_buddy.utils.common import *
 
 def test_time_format_seconds():
@@ -47,3 +49,134 @@ def test_time_format_zero():
     assert hours == 0
     assert minutes == 0
     assert seconds == 0
+
+def test_pretty_time_format_seconds():
+    seconds = 0
+    time = pretty_time_format(seconds)
+    expected = "0 seconds"
+    assert time == expected
+    time = pretty_time_format(seconds, shorten=True)
+    expected = "0s"
+    assert time == expected
+
+    # Singular
+    seconds = 1
+    time = pretty_time_format(seconds)
+    expected = "1 second"
+    assert time == expected
+
+    # Plural
+    seconds = seconds * 2
+    time = pretty_time_format(seconds)
+    expected = "2 seconds"
+    assert time == expected
+
+def test_pretty_time_format_minutes():
+    # Singular
+    seconds = 60
+    time = pretty_time_format(seconds)
+    expected = "1 minute"
+    assert time == expected
+
+    # Plural
+    seconds = seconds * 2
+    time = pretty_time_format(seconds)
+    expected = "2 minutes"
+    assert time == expected
+
+    time = pretty_time_format(seconds, shorten=True)
+    expected = "2m"
+    assert time == expected
+
+    time = pretty_time_format(seconds, always_seconds=True)
+    expected = "2 minutes 0 seconds"
+    assert time == expected
+
+    time = pretty_time_format(seconds, shorten=True, always_seconds=True)
+    expected = "2m 0s"
+    assert time == expected
+
+def test_pretty_time_format_hours():
+    # Singular
+    seconds = 3600
+    time = pretty_time_format(seconds)
+    expected = "1 hour"
+    assert time == expected
+
+    # Plural
+    seconds = seconds * 2
+    time = pretty_time_format(seconds)
+    expected = "2 hours"
+    assert time == expected
+
+    time = pretty_time_format(seconds, shorten=True)
+    expected = "2h"
+    assert time == expected
+
+    time = pretty_time_format(seconds, always_seconds=True)
+    expected = "2 hours 0 seconds"
+    assert time == expected
+
+    time = pretty_time_format(seconds, shorten=True, always_seconds=True)
+    expected = "2h 0s"
+    assert time == expected
+
+def test_pretty_time_format_days():
+    # Singular
+    seconds = 86400
+    time = pretty_time_format(seconds)
+    expected = "1 day"
+    assert time == expected
+
+    # Plural
+    seconds = seconds * 2
+    time = pretty_time_format(seconds)
+    expected = "2 days"
+    assert time == expected
+
+    time = pretty_time_format(seconds, shorten=True)
+    expected = "2d"
+    assert time == expected
+
+    time = pretty_time_format(seconds, always_seconds=True)
+    expected = "2 days 0 seconds"
+    assert time == expected
+
+    time = pretty_time_format(seconds, shorten=True, always_seconds=True)
+    expected = "2d 0s"
+    assert time == expected
+
+def test_time_format():
+    # Singular
+    seconds = 90061
+    time = pretty_time_format(seconds)
+    expected = "1 day 1 hour 1 minute"
+    assert time == expected
+    time = pretty_time_format(seconds, always_seconds=True)
+    expected = "1 day 1 hour 1 minute 1 second"
+    assert time == expected
+
+    # Plural
+    seconds = seconds * 2
+    time = pretty_time_format(seconds)
+    expected = "2 days 2 hours 2 minutes"
+    assert time == expected
+    time = pretty_time_format(seconds, always_seconds=True)
+    expected = "2 days 2 hours 2 minutes 2 seconds"
+    assert time == expected
+
+    time = pretty_time_format(seconds, shorten=True)
+    expected = "2d 2h 2m"
+    assert time == expected
+
+    time = pretty_time_format(seconds, shorten=True, always_seconds=True)
+    expected = "2d 2h 2m 2s"
+    assert time == expected
+
+    time = pretty_time_format(seconds, only_most_significant=True)
+    expected = "2 days"
+    assert time == expected
+
+    time = pretty_time_format(seconds, shorten=True, only_most_significant=True)
+    expected = "2d"
+    assert time == expected
